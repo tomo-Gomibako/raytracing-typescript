55f7337ac0bb057297f8c6d59bd836f05ddcb72f
tomon9086 <tomo.mouse.9086@gmail.com>
2019-06-19 12:40:43 +0900

    :sparkles:Vector3クラス完成

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,27 @@ import Vector3 from "./modules/Vector3"
 
 const v1 = new Vector3(1, 2, 3)
 const v2 = new Vector3(3, 5, 7)
-// const v2 = { x: 3, y: 5, z: 7 }
 
 console.log(v1.tos())
+console.log(v2.tos())
+
+console.log(v1.normalized.tos())
+console.log(v1.normalized.norm)
+
+console.log(v1.add(v2).tos())
+
+console.log(v1.subtract(v2).tos())
+
+console.log(v1.multiply(v2).tos())
 console.log(v1.multiply(2).tos())
+
+console.log(v1.devide(v2).tos())
+console.log(v1.devide(2).tos())
+
 console.log(v1.dot(v2))
+console.log(v1.cross(v2))
+console.log(v1.sq_magnitude())
+console.log(v1.magnitude())
+console.log(v1.normalize())
+console.log(v1.equals(new Vector3(1, 2, 3)))
+console.log(v1.equals(v2))
--- src/modules/Vector3.ts
+++ src/modules/Vector3.ts
@@ -7,14 +7,20 @@ class Vector3 {
 		this.y = y
 		this.z = z
 	}
-	get normalized(): number {
-		return 0
+	get normalized(): Vector3 {
+		return this.normalize()
 	}
 	get norm(): number {
-		return 0
+		return this.magnitude()
 	}
 	tos(): string {
 		return `Vector3(${ this.x }, ${ this.y }, ${ this.z })`
+	}
+	add(vec: Vector3): Vector3 {
+		return new Vector3(this.x + vec.x, this.y + vec.y, this.z + vec.z)
+	}
+	subtract(vec: Vector3): Vector3 {
+		return new Vector3(this.x - vec.x, this.y - vec.y, this.z - vec.z)
 	}
 	multiply(val: Vector3): Vector3
 	multiply(val: number): Vector3
@@ -27,24 +33,44 @@ class Vector3 {
 			throw new Error("An argument must be number or vector3.")
 		}
 	}
+	devide(val: Vector3): Vector3
+	devide(val: number): Vector3
+	devide(val: any): Vector3 {
+		if(val instanceof Vector3) {
+			return new Vector3(this.x / val.x, this.y / val.y, this.z / val.z)
+		} else if(typeof val === "number") {
+			return new Vector3(this.x / val, this.y / val, this.z / val)
+		} else {
+			throw new Error("An argument must be number or vector3.")
+		}
+	}
 	dot(vec: Vector3): number {
 		return this.x * vec.x + this.y * vec.y + this.z * vec.z
 	}
+	cross(vec: Vector3): Vector3 {
+		return new Vector3(
+			this.y * vec.z + this.z * vec.y,
+			this.z * vec.x + this.x * vec.z,
+			this.x * vec.y + this.y * vec.x
+		)
+	}
+	sq_magnitude(): number {
+		return this.dot(this)
+	}
+	magnitude(): number {
+		return Math.sqrt(this.sq_magnitude())
+	}
+	normalize(): Vector3 {
+		const mag: number = this.magnitude()
+		return new Vector3(this.x / mag, this.y / mag, this.z / mag)
+	}
+	equals(vec: Vector3): boolean {
+		if(this.x == vec.x && this.y == vec.y && this.z == vec.z) {
+			return true
+		} else {
+			return false
+		}
+	}
 }
-
-/*
-getters:
-	normalized
-	norm
-
-methods:
-	tos
-	dot
-	cross
-	sq_magnitude
-	magnitude
-	normalize
-	equals
-*/
 
 export default Vector3
